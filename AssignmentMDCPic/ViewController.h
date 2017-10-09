2fb1dd4807bd21878c1045ba44de95682df4bd6e
user <user@w347437.local>
2017-10-09 18:01:01 -0400

    Initial Commit

---
+++ AssignmentMDCPic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AssignmentMDCPic
+//
+//  Created by user on 10/9/17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
